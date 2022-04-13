facf31f130fa502e37f4631b6d0d584b67e85e41
Paulo Alexandre <paulo__alexandre.souza@hotmail.com>
2022-04-13 14:47:51 -0300

    modularizacao e service model

---
+++ src/app/model/quadra.model.ts
@@ -0,0 +1,15 @@
+export class Quadra {
+    public id: number;
+    public nome: string;
+    public logradouro: string;
+    public bairro: string;
+    public cidade: string;
+    public esporte: string;
+    public valorHora: number;
+    public telefone: string;
+    public descricao: string;
+
+    constructor() {
+
+    }
+}
--- src/app/services/quadras.service.ts
+++ src/app/services/quadras.service.ts
@@ -1,9 +1,83 @@
 import { Injectable } from '@angular/core';
+import { Quadra } from '../model/quadra.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuadrasService {
 
+  public quadras: Array<Quadra> = [
+    {
+      id: 0,
+      nome: 'Quadra dos Pobres',
+      logradouro: 'rua dos pobres 666',
+      bairro: 'mae dos pobres',
+      cidade: 'Belo Horizonte',
+      esporte: 'Futebol',
+      valorHora: 150,
+      telefone: '98888888',
+      descricao: 'Uma quadra esportiva perfeita para sua diversão'
+    },
+    {
+      id: 1,
+      nome: 'Quadra dos Ricos',
+      logradouro: 'rua dos ricos 555',
+      bairro: 'mae dos ricos',
+      cidade: 'Belo Horizonte',
+      esporte: 'Futebol',
+      valorHora: 5000,
+      telefone: '977777777',
+      descricao: 'Uma quadra esportiva perfeita para sua diversão'
+    },
+    {
+      id: 2,
+      nome: 'Quadra dos Sem Ideia',
+      logradouro: 'Sem ideia 444',
+      bairro: 'mae dos sem ideia',
+      cidade: 'Belo Horizonte',
+      esporte: 'volei',
+      valorHora: 50,
+      telefone: '966666666',
+      descricao: 'Uma quadra esportiva perfeita para sua diversão'
+    }
+  ]
+
   constructor() { }
+
+  public getAll() {
+    return this.quadras;
+  }
+
+  public getById(id: number) {
+    for (let obj of this.quadras) {
+      if (obj.id === id) {
+        return obj;
+      }
+    }
+  }
+
+  public add(quadra: Quadra) {
+    quadra.id = this.quadras.length;
+    this.quadras.push(quadra);
+  }
+
+  public edit(quadra: Quadra) {
+    const id = quadra.id;
+
+    for (let obj of this.quadras) {
+      if (obj.id === id) {
+        obj.nome = quadra.nome;
+        obj.logradouro = quadra.logradouro;
+        obj.bairro = quadra.bairro;
+        obj.cidade = quadra.cidade;
+        obj.esporte = quadra.esporte;
+        obj.valorHora = quadra.valorHora;
+        obj.telefone = quadra.telefone;
+        obj.descricao = quadra.descricao;
+        break;
+      }
+    }
+  }
 }
+
+
